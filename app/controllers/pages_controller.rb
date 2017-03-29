4cbc9c82c2d49719814e8320f81f6c0621dd6d90
Juan Carlos Vergara <juanvergara9@hotmail.com>
2017-03-28 22:22:39 -0300

    first commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def x
   end
+  def save_user
+  	unless params[:name].blank? or params[:email].blank? or params[:age].blank?
+
+  		User.create(name:params[:name], email:params[:email], age:params[:age])
+  		redirect_to pages_x_path,
+  		notice:"Usuario creado correctamente! "
+  		
+  	end
+  	
+  end
+
 end
